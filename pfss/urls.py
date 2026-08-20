e5b6f4ffb00c8e2110700ec3486c192c613321a0
coombescody2-debug <coombes.cody2@gmail.com>
2026-08-20 14:02:19 -0500

    Update urls.py

--- pfss/urls.py
+++ pfss/urls.py
@@ -3,22 +3,19 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from pfss import views as pfss_views
-
 from django.contrib import admin
-
 from . import views
 
-prefix=""
-#prefix='pfs/' # used to run with runserver
 urlpatterns = [
-    url(r"^%s$" % prefix, TemplateView.as_view(template_name="homepage.html"), name="home"),
+    url(r'^$', TemplateView.as_view(template_name="homepage.html"), name="home"),
     url(r'^admin/', admin.site.urls),
-    url(r"^%saccount/" % prefix, include("account.urls")),
-    url(r'^%slist/handle/$' % prefix, pfss_views.handleList),
-    url(r'^%slist/$' % prefix, pfss_views.creatureList),
-    url(r'^%slist/(?P<group_ID>\d+)/$' % prefix, pfss_views.creatureList),
-    url(r'^%slist/code/(?P<code>\w+)/$' % prefix, pfss_views.creatureListByCode),
-    url(r'^%screature/(\d+)/$' % prefix, pfss_views.creatureView),
+    url(r'^account/', include("account.urls")),
+    url(r'^slist/handle/$', pfss_views.handleList),
+    url(r'^slist/$', pfss_views.creatureList),
+    url(r'^slist/(?P<group_ID>\d+)/$', pfss_views.creatureList),
+    url(r'^slist/code/(?P<code>\w+)/$', pfss_views.creatureListByCode),
+    url(r'^creature/(\d+)/$', pfss_views.creatureView),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
