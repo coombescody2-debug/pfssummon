8ccebb4b623e62b162d97117fbdcf5bc75777476
coombescody2-debug <coombes.cody2@gmail.com>
2026-08-20 14:39:34 -0500

    Update wsgi.py

--- pfss/wsgi.py
+++ pfss/wsgi.py
@@ -1,17 +1,19 @@
-"""
-WSGI config for pfss project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pfss.settings")
 
+# Securely initialize the Django App Registry first
 application = get_wsgi_application()
+
+# Now that the registry is ready, safely patch the missing functions
+try:
+    import django.contrib.sites.shortcuts
+    import django.contrib.sites.models
+    
+    # Append the missing shortcut function to the real models file
+    django.contrib.sites.models.get_current_site = django.contrib.sites.shortcuts.get_current_site
+    sys.modules['django.contrib.sites.models'].get_current_site = django.contrib.sites.shortcuts.get_current_site
+except Exception:
+    pass
